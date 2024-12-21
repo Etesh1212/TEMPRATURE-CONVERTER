77b8a31bb71e2243a7bf2b4c7dbe3f04b2552ebe
Etesh1212 <166606783+Etesh1212@users.noreply.github.com>
2024-12-21 21:12:22 +0530

    TEMPRATURE CONVERTER

---
+++ index.js
@@ -0,0 +1,27 @@
+const calcius = document.querySelector("#calcius");
+const fahrenheit = document.querySelector("#farenheit");
+const kelvin = document.querySelector("#kelvin");
+
+calcius.addEventListener("input", function () {
+  let c = parseFloat(calcius.value);
+  let f = (c * 9) / 5 + 32;
+  let k = c + 273.15;
+  fahrenheit.value = f;
+  kelvin.value = k;
+});
+
+calcius.addEventListener("input", function () {
+  let c = parseFloat(calcius.value);
+  let f = (c * 9) / 5 + 32;
+  let k = c + 273.15;
+  fahrenheit.value = f;
+  kelvin.value = k;
+});
+
+calcius.addEventListener("input", function () {
+  let c = parseFloat(calcius.value);
+  let f = (c * 9) / 5 + 32;
+  let k = c + 273.15;
+  fahrenheit.value = f;
+  kelvin.value = k;
+});
